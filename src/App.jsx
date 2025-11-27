79019d2b997b8013e570876b068a4f6a67d59fc4
Zacarias2025-spec <saramendes.ang2025@gmail.com>
2025-11-27 12:30:29 +0100

    Initialize App component with routing setup

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Feed from "./pages/Feed";
+import Profile from "./pages/Profile";
+import Chat from "./pages/Chat";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="/chat/:id" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
